ec4e975c8fefc691ae2044570380f72534e8d994
ggalvisgg <gabrielagalvisgg@gmail.com>
2025-05-15 22:55:06 -0500

    Se actualizaron otra vez las importaciones

--- repositories/book_repository.go
+++ repositories/book_repository.go
@@ -20,19 +20,27 @@ type BookRepository struct {
 // repositories/book_repository.go
 func (r *BookRepository) GetAllBooks() ([]models.Book, error) {
     var books []models.Book
-    cursor, err := r.collection.Find(context.TODO(), bson.M{})
-    if err != nil {
-        return nil, err
-    }
-    defer cursor.Close(context.TODO())
 
-    for cursor.Next(context.TODO()) {
-        var book models.Book
-        if err := cursor.Decode(&book); err != nil {
-            return nil, err
-        }
-        books = append(books, book)
+    books := []models.Book{
+        {
+            ID:     "1",
+            Title:  "Libro de ejemplo",
+            Author: "Autora X",
+        },
     }
+    // cursor, err := r.collection.Find(context.TODO(), bson.M{})
+    // if err != nil {
+    //     return nil, err
+    // }
+    // defer cursor.Close(context.TODO())
+
+    // for cursor.Next(context.TODO()) {
+    //     var book models.Book
+    //     if err := cursor.Decode(&book); err != nil {
+    //         return nil, err
+    //     }
+    //     books = append(books, book)
+    // }
 
     return books, nil
 }
